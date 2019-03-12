43e85e7e5f76324b7c891d7d896599035a437f03
Alexandr Yaroshevich <33527612+AYaro@users.noreply.github.com>
2019-03-12 13:02:05 +0300

    Create 2.py

---
+++ week1/2.py
@@ -0,0 +1,6 @@
+inputFile = open("input.txt", "r")
+a,b = [int(x) for x in inputFile.readline().split()]
+inputFile.close()
+outputFile = open("output.txt", "w")
+outputFile.write(str(a+b*b))
+outputFile.close()
